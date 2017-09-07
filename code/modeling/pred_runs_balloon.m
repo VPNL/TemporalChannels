7131f544edede3fdf6b588c26c714d51edc175ea
astigliani <astiglia@stanford.edu>
2017-09-07 16:44:31 -0700

    Added implementation of balloon model

--- code/modeling/pred_runs_balloon.m
+++ code/modeling/pred_runs_balloon.m
@@ -25,31 +25,44 @@ run_preds(empty_cells) = {[]};
 
 % get the simulated values of all variables for each predictor in each run
 for ss = 1:nsess
-    for rr = 1:nruns_max
-        for pp = 1:size(in_flow{rr, ss}, 2)
-            % initialize variables
-            [v, q, IN_FLOW, OUT_FLOW, CMRO2] = deal(1);
-            S = 0; OEF = params.E0; t = time_vecs{rr, ss}; 
-            which_tau = 1; tau = params.tau_i; tauMTT = params.tauMTT;
-            tau_p = params.tau_p; tau_n = params.tau_n;
-            E0 = params.E0; V0 = params.V0; alpha = params.alpha;
-            % get the simulated values of all variables
-            for ii = 1:size(time_vecs{rr, ss}, 1) - 1
-                ii_flow = in_flow{rr, ss}(ii, pp);
-                v(ii + 1) = runge_kutta(dt, @dvdt, t(ii), v(ii), ii_flow);
-                OUT_FLOW(ii + 1) = flow_out(v(ii + 1), t(ii), ii_flow);
-                q(ii + 1) = runge_kutta(dt, @dqdt, t(ii), q(ii), v(ii), ii_flow);  
-                S1 = params.k1 * (1 - q(ii + 1));
-                S2 = params.k2 * (1 - (q(ii + 1) / v(ii + 1)));
-                S3 = params.k3 * (1 - v(ii + 1));
-                S(ii + 1) = V0 * (S1 + S2 + S3);
-                OEF(ii + 1) = 1 - (1 - E0) .^ (1. / ii_flow);
-                CMRO2(ii + 1) = (OEF(ii + 1) / E0) * IN_FLOW(ii);
-                IN_FLOW(ii + 1) = ii_flow;
+    [~, session_id] = fileparts(model.sessions{ss});
+    fprintf('Simulating balloon model for %s', session_id);
+    fname = ['balloon_model_' [model.experiments{:}] '.mat'];
+    fpath = fullfile(model.sessions{ss}, 'Stimuli', fname);
+    if exist(fpath, 'file') == 2
+        load(fpath);
+        run_preds(:, ss) = session_run_preds;
+    else
+        for rr = 1:nruns_max
+            fprintf('.');
+            for pp = 1:size(in_flow{rr, ss}, 2)
+                % initialize variables
+                [v, q, IN_FLOW, OUT_FLOW, CMRO2] = deal(1);
+                S = 0; OEF = params.E0; t = time_vecs{rr, ss}; 
+                which_tau = 1; tau = params.tau_i; tauMTT = params.tauMTT;
+                tau_p = params.tau_p; tau_n = params.tau_n;
+                E0 = params.E0; V0 = params.V0; alpha = params.alpha;
+                % get the simulated values of all variables
+                for ii = 1:size(time_vecs{rr, ss}, 1) - 1
+                    ii_flow = in_flow{rr, ss}(ii, pp);
+                    v(ii + 1) = runge_kutta(dt, @dvdt, t(ii), v(ii), ii_flow);
+                    OUT_FLOW(ii + 1) = flow_out(v(ii + 1), t(ii), ii_flow);
+                    q(ii + 1) = runge_kutta(dt, @dqdt, t(ii), q(ii), v(ii), ii_flow);  
+                    S1 = params.k1 * (1 - q(ii + 1));
+                    S2 = params.k2 * (1 - (q(ii + 1) / v(ii + 1)));
+                    S3 = params.k3 * (1 - v(ii + 1));
+                    S(ii + 1) = V0 * (S1 + S2 + S3);
+                    OEF(ii + 1) = 1 - (1 - E0) .^ (1. / ii_flow);
+                    CMRO2(ii + 1) = (OEF(ii + 1) / E0) * IN_FLOW(ii);
+                    IN_FLOW(ii + 1) = ii_flow;
+                end
+                Sr = convolve_vecs(S(1:rd{rr, ss} / params.delta_t)', 1, fs, 1 / tr);
+                run_preds{rr, ss}(:, pp) = Sr;
             end
-            Sr = convolve_vecs(S(1:rd{rr, ss} / params.delta_t)', 1, fs, 1 / tr);
-            run_preds{rr, ss}(:, pp) = Sr;
         end
+        session_run_preds = run_preds(:, ss); 
+        save(fpath, 'session_run_preds', '-v7.3');
+        fprintf('\n');
     end
 end
 run_preds(empty_cells) = {[]};
