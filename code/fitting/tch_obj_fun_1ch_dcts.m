44bde65d1d9daacac2ab734cbc559956620dc26f
astigliani <astiglia@stanford.edu>
2017-11-04 20:55:21 -0700

    Added documentation of objective functions

--- code/fitting/tch_obj_fun_1ch_dcts.m
+++ code/fitting/tch_obj_fun_1ch_dcts.m
@@ -16,24 +16,33 @@ function obj_fun = tch_obj_fun_1ch_dcts(roi, model)
 if ~strcmp(model.type, '1ch-dcts'); error('Incompatible model type'); end
 stim = model.stim; nruns = size(stim, 1); irfs = model.irfs; fs = model.fs;
 run_avgs = roi.run_avgs; baseline = roi.baseline; tr = roi.tr;
-
+% generate IRFs/filters for optimization
 irf_fun = @(x) (0:999) .* exp(-(0:999) / (x * 1000));
 div_lpf = @(f) exp(-(0:999) / f) / sum(exp(-(0:999) / f));
+% linear response: stim * IRF[tau1]
 conv_sn = @(x, y) cellfun(@(X, Y) convolve_vecs(X, Y, 1, 1), ...
     x, repmat({irf_fun(y)}, nruns, 1), 'uni', false);
+% filtered response: (linear response * low-pass filter[tau2])^2
 conv_nf = @(x, y, f) cellfun(@(X, F) convolve_vecs(X, F, fs, fs) .^ 2, ...
     conv_sn(x, y), repmat({div_lpf(f)}, nruns, 1), 'uni', false);
+% neural response: (linear response)^2 / (sigma^2 + filtered response)
 comp_dn = @(x, y, z, f) cellfun(@(N, F, Z) (N .^ 2) ./ (F + Z .^ 2), ...
     conv_sn(x, y), conv_nf(x, y, f), repmat({z}, nruns, 1), 'uni', false);
+% bold response: neural response * HRF
 conv_nb = @(x, y, z, f) cellfun(@(N) convolve_vecs(N, irfs.hrf{1}, fs, 1 / tr), ...
     comp_dn(x, y, z, f), 'uni', false);
+% measured signal: time series - baseline estimates
 comp_bs = @(m, b0) cellfun(@(M, B0) M - repmat(B0, size(M, 1), 1), ...
     m, b0, 'uni', false);
+% channel weights: bold response \ measured signal
 comp_ws = @(x, y, z, f, m, b0) cell2mat(conv_nb(x, y, z, f)) \ cell2mat(comp_bs(m, b0));
+% predicted signal: bold response x channel weights
 pred_bs = @(x, y, z, f, m, b0) cellfun(@(P, W) P .* repmat(W, size(P, 1), 1), ...
     conv_nb(x, y, z, f), repmat({comp_ws(x, y, z, f, m, b0)'}, nruns, 1), 'uni', false);
+% model residuals: (predicted signal - measured signal)^2
 calc_br = @(x, y, z, f, m, b0) cellfun(@(S, M) (sum(S, 2) - M) .^ 2, ...
     pred_bs(x, y, z, f, m, b0), comp_bs(m, b0), 'uni', false);
+% model error: summed squared residuals for all run time series
 calc_me = @(x, y, z, f, m, b0) sum(cell2mat(calc_br(x, y, z, f, m, b0)));
 obj_fun = @(x) calc_me(stim, x(1), x(2), x(3), run_avgs, baseline);
 
--- code/fitting/tch_obj_fun_3ch_lin_rect_exp.m
+++ code/fitting/tch_obj_fun_3ch_lin_rect_exp.m
@@ -18,30 +18,43 @@ if ~strcmp(model.type, '3ch-lin-rect-exp'); error('Incompatible model type'); en
 stim = model.stim; nruns = size(stim, 1); irfs = model.irfs; fs = model.fs;
 run_avgs = roi.run_avgs; baseline = roi.baseline; tr = roi.tr;
 
+% generate IRFs/filters for optimization
 delay_fun = @(y) exp(-(1:12000) / (y * 1000));
+% sustained response: stim * sustained IRF
 conv_snS = @(x) cellfun(@(X) convolve_vecs(X, irfs.nrfS{1}, 1, 1), ...
     x, 'uni', false);
+% transient response: max(0, (stim * transient IRF)^2)
 conv_snT = @(x) cellfun(@(X) rectify(convolve_vecs(X, irfs.nrfT{1}, 1, 1), 'positive'), ...
     x, 'uni', false);
+% delay response: stim x exponential filter
 doffsets = cellfun(@(X, Y) [X(2:end) Y], model.onsets, model.run_durs, 'uni', false);
 conv_snD = @(x, y) cellfun(@(X, Y, ON, OFF) code_exp_decay(X, ON, OFF, Y, fs), ...
     cellfun(@code_delay_act, x, 'uni', false), repmat({delay_fun(y)}, nruns, 1), ...
     model.offsets, doffsets, 'uni', false);
+% sustained BOLD: sustained response * HRF
 conv_nbS = @(x) cellfun(@(NS) convolve_vecs(NS, irfs.hrf{1}, fs, 1 / tr), ...
     conv_snS(x), 'uni', false);
+% transient BOLD: transient response * HRF
 conv_nbT = @(x) cellfun(@(NT) convolve_vecs(NT, irfs.hrf{1}, fs, 1 / tr), ...
     conv_snT(x), 'uni', false);
+% delay BOLD: delay response * HRF
 conv_nbD = @(x, y) cellfun(@(ND) convolve_vecs(ND, irfs.hrf{1}, fs, 1 / tr), ...
     conv_snD(x, y), 'uni', false);
+% channel predictors: [sustained BOLD, transient BOLD, delay BOLD]
 conv_nb = @(x, y) cellfun(@(S, T, D) [S T D], ...
     conv_nbS(x), conv_nbT(x), conv_nbD(x, y), 'uni', false);
+% measured signal: time series - baseline estimates
 comp_bs = @(m, b0) cellfun(@(M, B0) M - repmat(B0, size(M, 1), 1), ...
     m, b0, 'uni', false);
+% channel weights: channel predictors \ measured signal
 comp_ws = @(x, y, m, b0) cell2mat(conv_nb(x, y)) \ cell2mat(comp_bs(m, b0));
+% predicted signal: channel predictors x channel weights
 pred_bs = @(x, y, m, b0) cellfun(@(P, W) P .* repmat(W, size(P, 1), 1), ...
     conv_nb(x, y), repmat({comp_ws(x, y, m, b0)'}, nruns, 1), 'uni', false);
+% model residuals: (predicted signal - measured signal)^2
 calc_br = @(x, y, m, b0) cellfun(@(S, M) (sum(S, 2) - M) .^ 2, ...
     pred_bs(x, y, m, b0), comp_bs(m, b0), 'uni', false);
+% model error: summed squared residuals for all run time series
 calc_me = @(x, y, m, b0) sum(cell2mat(calc_br(x, y, m, b0)));
 obj_fun = @(x) calc_me(stim, x(1), run_avgs, baseline);
 
