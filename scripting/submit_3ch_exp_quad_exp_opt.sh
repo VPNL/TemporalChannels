2a4a73d60cbffb117a4e3f3f00aeeab50f289094
astigliani <astiglia@stanford.edu>
2018-01-09 11:00:53 -0800

    Additional model implementations

---
+++ scripting/submit_3ch_exp_quad_exp_opt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#SBATCH --job-name=c3eqeo
+#
+#SBATCH --time=48:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem-per-cpu=2GB
+#SBATCH -p hns,normal
+
+module load matlab/R2017a
+matlab -nodisplay < optimize_3ch_exp_quad_exp_opt.m
