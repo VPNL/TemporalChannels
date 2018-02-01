51b4fda4b9fc81401cf46199123cbe12e8dcf8a6
astigliani <astiglia@stanford.edu>
2018-02-01 14:01:00 -0800

    Additional model implementations

---
+++ scripting/submit_2ch_exp_cquad.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#SBATCH --job-name=c2ecq
+#
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem-per-cpu=2GB
+#SBATCH -p hns,normal
+
+module load matlab/R2017a
+matlab -nodisplay < optimize_2ch_exp_cquad.m
